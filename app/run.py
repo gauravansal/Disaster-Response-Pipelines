5ee4f93ae9d3a47c1e289c4a03f8cbc3c6eea19e
Gaurav Ansal <ansal.gaurav@gmail.com>
2019-04-26 07:16:27 -0500

    docs: Add docstring to custom estimator

--- app/run.py
+++ app/run.py
@@ -68,6 +68,12 @@ def tokenize(text):
 
 # Add custom Estimator
 class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+	"""
+    Starting Verb Extractor class
+    
+    This class extract the starting verb of a sentence,
+    creating a new feature for the ML classifier
+    """
 
     def starting_verb(self, text):
         sentence_list = nltk.sent_tokenize(text)
